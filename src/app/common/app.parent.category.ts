c53678531b2c0c314886132c46684e50905052ba
sush154 <sushant.pj41288@gmail.com>
2017-12-22 19:02:58 +0530

    Office commit

--- src/app/common/app.parent.category.ts
+++ src/app/common/app.parent.category.ts
@@ -1,6 +1,13 @@
-export const PARENTCATEGORY = [
+/*export const PARENTCATEGORY = [
     {"id" : "1", "value" : "Dress Material"},
     {"id" : "2", "value" : "Saree"},
     {"id" : "3", "value" : "Kurtis"},
     {"id" : "4", "value" : "Lehengas"}
+];*/
+
+export const PARENTCATEGORY = [
+    {"id" : "1", "value" : "PC1"},
+    {"id" : "2", "value" : "PC2"},
+    {"id" : "3", "value" : "PC3"},
+    {"id" : "4", "value" : "PC4"}
 ];
